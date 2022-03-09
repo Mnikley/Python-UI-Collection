39515489ece4f02982488058e6cdce28b1284aee
Matthias Ley <75040444+Mnikley@users.noreply.github.com>
2022-03-09 16:52:28 +0100

    Added minimal tksheet example

---
+++ TKinter/Snippets/table_app_lite_minimal.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tksheet import Sheet
+
+
+class demo(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.grid_columnconfigure(0, weight = 1)
+        self.grid_rowconfigure(0, weight = 1)
+        self.frame = tk.Frame(self)
+        self.frame.grid_columnconfigure(0, weight = 1)
+        self.frame.grid_rowconfigure(0, weight = 1)
+        self.sheet = Sheet(self.frame,
+                           data = [[f"Row {r}, Column {c}\nnewline1\nnewline2" for c in range(50)] for r in range(500)])
+        self.sheet.enable_bindings()
+        self.frame.grid(row = 0, column = 0, sticky = "nswe")
+        self.sheet.grid(row = 0, column = 0, sticky = "nswe")
+
+
+app = demo()
+app.mainloop()
